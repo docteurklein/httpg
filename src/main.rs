ef3750afd2a03de2b361a1999e7dbb687eff0d02
Florian Klein <florian.klein@free.fr>
2024-10-28 16:17:05 +0100

    biscuit!

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,39 @@
 use axum::{
-    extract::{Json, Path, Query, State},
+    extract::{Json, Query, State},
     http::{
-        header::{HeaderMap, HeaderValue, ACCEPT, AUTHORIZATION},
-        Method, StatusCode,
+        header::{HeaderMap, ACCEPT, AUTHORIZATION},
+        StatusCode,
     },
     response::{Html, IntoResponse, Response},
-    routing::{get, post},
-    Router,
+    routing::post,
+    Router
 };
+use axum_extra::TypedHeader;
+use headers::{Authorization, authorization::Bearer};
+
 use axum_macros::debug_handler;
+use tokio::fs;
 use tower::builder::ServiceBuilder;
 use tower_http::{cors::{Any, CorsLayer}, services::ServeDir};
 // use quaint::{val, col, Value};
 use handlebars::Handlebars;
 use serde::{Deserialize, Serialize};
 use serde_json::{json, Value};
-use std::cmp::min;
 use std::collections::HashMap;
 use std::env;
-use std::fs;
 use std::net::SocketAddr;
-use std::time::Duration;
 use tokio_postgres::{NoTls, Error, Client};
 use tokio_postgres::types::{FromSql, ToSql, Type};
 use deadpool_postgres::{Config, ManagerConfig, Pool, RecyclingMethod, Runtime};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use biscuit_auth::{KeyPair, PrivateKey, builder_ext::AuthorizerExt, error, macros::*, Biscuit};
 
-#[derive(Clone, Debug, Serialize, Deserialize, FromSql)]
-struct Link {
-    target: String,
-    attributes: HashMap<String, Option<String>>,
-}
-
-#[derive(Clone, Debug, Serialize, Deserialize, FromSql)]
-struct RelationDef {
-    fqn: String,
-    alias: String,
-    nspname: String,
-    relname: String,
-    cols: Value,
-    in_links: Value,
-    out_links: Value,
-}
-
-#[derive(Clone, Debug, Deserialize)]
-struct Procedure {
-    escaped_fqn: String,
-    arguments: Value,
-}
 
 #[derive(Clone)]
 struct AppState {
     pool: Pool,
-    relations: HashMap<String, RelationDef>,
-    procedures: HashMap<String, Procedure>,
-    max_limit: usize,
-    max_offset: usize,
-    anon_role: String,
-}
-
-#[derive(Debug, Deserialize)]
-struct Params {
-    limit: Option<usize>,
-    offset: Option<usize>,
+    // anon_role: String,
+    private_key: PrivateKey,
 }
 
 #[tokio::main]
@@ -75,29 +46,22 @@ async fn main() {
         .init();
 
     let mut cfg = Config::new();
-    cfg.user = Some("Florian-Klein".to_string());
+    cfg.user = Some("florian".to_string());
     cfg.dbname = Some("httpg".to_string());
-    cfg.host = Some("/run/user/1001/devenv-95a9151/postgres/".to_string());
+    cfg.host = Some("/run/user/1000/devenv-da8fc55/postgres/".to_string());
     cfg.manager = Some(ManagerConfig {
         recycling_method: RecyclingMethod::Fast,
     });
     let pool = cfg.create_pool(Some(Runtime::Tokio1), NoTls).unwrap();
 
+    let pkey = fs::read(env::var("HTTPG_PRIVATE_KEY").expect("HTTPG_PRIVATE_KEY")).await.unwrap();
+    let pkey = hex::decode(pkey).unwrap();
+
     let state = AppState {
         pool,
-        max_limit: env::var("HTTPG_MAX_LIMIT")
-            .unwrap_or("100".to_string())
-            .parse::<usize>()
-            .unwrap(),
-        max_offset: env::var("HTTPG_MAX_OFFSET")
-            .unwrap_or("0".to_string())
-            .parse::<usize>()
-            .unwrap(),
-        anon_role: env::var("HTTPG_ANON_ROLE").expect("HTTPG_ANON_ROLE"),
-        relations: HashMap::new(),
-        procedures: HashMap::new(),
+    //     anon_role: env::var("HTTPG_ANON_ROLE").expect("HTTPG_ANON_ROLE"),
+        private_key: PrivateKey::from_bytes(&pkey).unwrap(),
     };
-    dbg!(&state.relations);
 
     let cors = CorsLayer::new()
         .allow_origin(Any);
@@ -107,7 +71,7 @@ async fn main() {
         .with_state(state)
         .layer(ServiceBuilder::new().layer(cors));
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3001));
     tracing::debug!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
@@ -121,16 +85,24 @@ struct QueryBody {
     params: Vec<String>,
 }
 
+#[debug_handler]
 async fn query(
-    State(AppState {pool, relations, max_limit, max_offset, anon_role, ..}): State<AppState>,
+    State(AppState {pool, private_key}): State<AppState>,
     headers: HeaderMap,
+    // TypedHeader(auth): TypedHeader<Authorization<Bearer>>,
     Query(qs): Query<HashMap<String, String>>,
     Json(body): Json<QueryBody>,
 ) -> Result<Response, (StatusCode, String)> {
-    let role = headers.get(AUTHORIZATION).and_then(|value| value.to_str().ok()).unwrap_or(&anon_role); // @TODO crypto
     let conn = pool.get().await.map_err(internal_error)?;
+    let root = KeyPair::from(&private_key);
 
-    // conn.execute("select set_config('role', $1, true)", &[&role]).await.map_err(internal_error)?;
+    let auth = headers.get(AUTHORIZATION).unwrap();
+    // dbg!(&auth, &root.public());
+    let biscuit = Biscuit::from_base64(&auth, root.public()).unwrap(); // map_err(internal_error)?;
+
+    let mut authorizer = biscuit.authorizer().unwrap();
+    let sql: Vec<(String, )> = authorizer.query("sql($sql) <- sql($sql)").unwrap();
+    conn.batch_execute(&sql.iter().map(|t| t.clone().0).collect::<Vec<String>>().join("; ")).await.map_err(internal_error)?;
 
     let sqlParams: Vec<&(dyn ToSql + Sync)> = body.params.iter().map(|x| x as &(dyn ToSql + Sync)).collect();
 
@@ -168,4 +140,32 @@ where
         StatusCode::INTERNAL_SERVER_ERROR,
         "internal error".to_string(),
     )
+}
+
+fn authorize(token: &Biscuit) -> Result<(), error::Token> {
+    let operation = "read";
+
+    // same as the `biscuit!` macro. There is also a `authorizer_merge!`
+    // macro for dynamic authorizer construction
+    let mut authorizer = authorizer!(
+      r#"operation({operation});"#
+    );
+
+    // register a fact containing the current time for TTL checks
+    authorizer.set_time();
+
+    // add a `allow if true;` policy
+    // meaning that we are relying entirely on checks carried in the token itself
+    authorizer.add_allow_all();
+
+    // link the token to the authorizer
+    authorizer.add_token(token)?;
+
+    let result = authorizer.authorize();
+
+    // store the authorization context
+    println!("{}", authorizer.to_base64_snapshot()?);
+
+    let _ = result?;
+    Ok(())
 }
