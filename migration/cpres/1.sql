f0838bf80a4830c9ce47eda30db017305d79150f
Florian Klein <florian.klein@free.fr>
2025-12-18 09:29:56 +0100

    migrate

--- migration/cpres/1.sql
+++ migration/cpres/1.sql
@@ -1,21 +1,56 @@
 
 \set ON_ERROR_STOP on
+\timing on
 
 set search_path to cpres;
+set lock_timeout to '50ms';
 
-begin; 
-alter table person drop constraint person_name_check;
-alter table person add constraint person_name_check  check (trim(name) <> '' and position('@' in name) = 0) not valid;
+select (
+    position('POSITION' in pg_get_constraintdef(oid, true)) = 0
+    or not convalidated
+) as needs_work
+from pg_constraint
+where conname = 'person_name_check'
+\gset
 
-alter table person drop constraint person_email_check;
-alter table person add constraint person_email_check  check (trim(email) <> '' and position('@' in email) <> 0) not valid;
+\if :needs_work
+    begin;
+    alter table person drop constraint person_name_check;
+    alter table person add constraint person_name_check  check (trim(name) <> '' and position('@' in name) = 0) not valid;
 
-commit;
+    alter table person drop constraint person_email_check;
+    alter table person add constraint person_email_check  check (trim(email) <> '' and position('@' in email) <> 0) not valid;
+    commit;
+\endif
 
-begin;
-set local lock_timeout to '50ms';
+do $$ begin
+perform 1;
+while FOUND loop
+    with r as materialized (
+        select person_id from person
+        where position('@' in name) <> 0
+        for no key update skip locked
+        limit 100
+    ),
+    u as (
+        update person
+        set name = replace(name, '@', '-at-')
+        from r
+        where person.person_id = r.person_id
+    )
+    select true into FOUND from r;
+    raise info '%', FOUND;
+    commit;
+end loop;
+end $$;
 
-alter table person validate constraint person_name_check;
-alter table person validate constraint person_email_check;
+\if :needs_work
+    begin;
+    alter table person validate constraint person_name_check;
+    commit;
 
-commit;
+    begin;
+    alter table person validate constraint person_email_check;
+    commit;
+\endif
+
