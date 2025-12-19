e67b5e79d838ca73fd1611fc02c66f693060fbbb
Florian Klein <florian.klein@free.fr>
2025-12-19 14:00:30 +0100

    force open window

--- public/service-worker.js
+++ public/service-worker.js
@@ -23,16 +23,9 @@ self.addEventListener('push', function(event) {
 });
 
 self.addEventListener('notificationclick', event => {
-  const rootUrl = new URL('/', location).href;
-  event.notification.close();
   event.waitUntil(
-    clients.matchAll().then(matchedClients => {
-      for (let client of matchedClients) {
-        if (client.url === rootUrl) {
-          return client.focus();
-        }
-      }
-      return clients.openWindow("/");
-    })
+    clients
+      .openWindow(self.location.origin)
+      .then(windowClient => windowClient.focus())
   );
 });
