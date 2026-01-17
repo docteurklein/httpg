46f122876565bce9a53bd1bbbc818d8236cd2fe4
Florian Klein <florian.klein@free.fr>
2026-01-17 15:49:48 +0100

    move pg rust stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+mod extract;
+mod sql;
+mod response;
+mod postgres;
+
+use crate::postgres::{DeadPoolConfig, NoCertificateVerification, PostgresConfig};
+
 use http::Uri;
 use axum::{
     Router, extract::{DefaultBodyLimit, State}, http::{
@@ -16,8 +23,6 @@ use lettre::{
     message::header::ContentType, transport::smtp::authentication::Credentials, AsyncSmtpTransport,
     AsyncTransport, Message, Tokio1Executor,
 };
-use rustls::{client::danger::{HandshakeSignatureValid, ServerCertVerified}, pki_types::{CertificateDer, ServerName, UnixTime}};
-use serde::{Deserialize, Serialize};
 use serde_json::json;
 use tokio_postgres_rustls::MakeRustlsConnect;
 use tower::builder::ServiceBuilder;
@@ -27,18 +32,14 @@ use core::{panic};
 use std::{fs::{self, File}, net::TcpListener, sync::Arc};
 use std::env;
 use std::net::SocketAddr;
-use tokio_postgres::{IsolationLevel, NoTls};
+use tokio_postgres::{IsolationLevel};
 use tokio_postgres::types::{ToSql, Type};
-use deadpool_postgres::{CreatePoolError, Pool, PoolError, Runtime, Transaction};
+use deadpool_postgres::{CreatePoolError, Pool, PoolError, Transaction};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 use biscuit_auth::{KeyPair, PrivateKey, Biscuit, builder::*};
 
 use crate::{extract::query::{Query}};
 use crate::response::compress_stream;
-
-mod extract;
-mod sql;
-mod response;
 
 #[derive(thiserror::Error, Debug)]
 pub enum HttpgError {
@@ -94,79 +95,6 @@ impl IntoResponse for HttpgError {
     fn into_response(self) -> Response {
         tracing::error!("{self:#?}");
         (StatusCode::INTERNAL_SERVER_ERROR, self.to_string()).into_response()
-    }
-}
-#[derive(Clone, Debug, Conf)]
-#[conf(env_prefix="PG_")]
-struct PostgresConfig {
-    #[conf(env)]
-    host: String,
-    #[conf(env)]
-    user: String,
-    #[conf(env, value_parser = |file: &str| -> Result<_, HttpgError> { Ok(fs::read_to_string(file)?) })]
-    password: String,
-    #[conf(env)]
-    dbname: String,
-}
-
-impl PostgresConfig {
-    pub fn from_env() -> tokio_postgres::Config {
-        let cfg = Self::parse();
-        tokio_postgres::Config::new()
-            .user(cfg.user)
-            .password(cfg.password)
-            .dbname(cfg.dbname)
-            .host(cfg.host)
-            .ssl_mode(match env::var("PG_SSLMODE").as_deref() {
-                Ok("require") => tokio_postgres::config::SslMode::Require,
-                _ => tokio_postgres::config::SslMode::Prefer,
-            })
-            .to_owned()
-    }
-}
-
-
-#[derive(Clone, Debug, Deserialize, Serialize)]
-struct DeadPoolConfig {
-    #[serde(default)]
-    pg: deadpool_postgres::Config,
-}
-
-impl DeadPoolConfig {
-    pub fn read() -> Result<Self, config::ConfigError> {
-        let config = config::Config::builder()
-            .add_source(config::Environment::default()
-                .prefix("PG")
-                .separator("_")
-                .keep_prefix(true)
-            )
-            .add_source(config::Environment::default()
-                .prefix("PG_READ")
-                .separator("_")
-                .keep_prefix(false)
-            )
-            .build()?
-        ;
-
-        config.try_deserialize::<Self>()
-    }
-
-    pub fn write() -> Result<Self, config::ConfigError> {
-        let config = config::Config::builder()
-            .add_source(config::Environment::default()
-                .prefix("PG")
-                .separator("_")
-                .keep_prefix(true)
-            )
-            .add_source(config::Environment::default()
-                .prefix("PG_WRITE")
-                .separator("_")
-                .keep_prefix(false)
-            )
-            .build()?
-        ;
-
-        config.try_deserialize::<Self>()
     }
 }
 
@@ -230,8 +158,8 @@ async fn main() -> Result<(), HttpgError> {
 
     let httpg_config = HttpgConfig::from_env()?;
 
-    let read_pool = create_pool(DeadPoolConfig::read()?, env::var("PG_SSLMODE").is_ok())?;
-    let write_pool = create_pool(DeadPoolConfig::write()?, env::var("PG_SSLMODE").is_ok())?;
+    let read_pool = DeadPoolConfig::read()?.create_pool()?;
+    let write_pool = DeadPoolConfig::write()?.create_pool()?;
     
     let state = AppState {
         read_pool,
@@ -260,7 +188,14 @@ async fn main() -> Result<(), HttpgError> {
     ;
     tokio::spawn(async move {
 
-        let (client, mut conn) = PostgresConfig::from_env().connect(NoTls).await?;
+        let cfg = PostgresConfig::from_env();
+        let tls_config = rustls::ClientConfig::builder()
+            .dangerous()
+            .with_custom_certificate_verifier(Arc::new(NoCertificateVerification {}))
+            .with_no_client_auth()
+        ;
+
+        let (client, mut conn) = cfg.connect(MakeRustlsConnect::new(tls_config)).await?;
 
         let mut stream = futures::stream::poll_fn(move |cx| conn.poll_message(cx));
 
@@ -322,22 +257,6 @@ async fn main() -> Result<(), HttpgError> {
         },
     };
     Ok(())
-}
-
-fn create_pool(cfg: DeadPoolConfig, is_ssl: bool) -> Result<Pool, HttpgError> {
-    if is_ssl {
-        let tls_config = rustls::ClientConfig::builder()
-            .dangerous()
-            .with_custom_certificate_verifier(Arc::new(NoCertificateVerification {}))
-            .with_no_client_auth()
-        ;
-        let tls = MakeRustlsConnect::new(tls_config);
-
-        cfg.pg.create_pool(Some(Runtime::Tokio1), tls).map_err(Into::into)
-    }
-    else {
-        cfg.pg.create_pool(Some(Runtime::Tokio1), NoTls).map_err(Into::into)
-    }
 }
 
 #[debug_handler]
@@ -744,53 +663,3 @@ async fn raw_http(
         rows: response::Rows::Raw(result)
     }.into_response())
 }
-
-#[derive(Debug)]
-pub struct NoCertificateVerification {}
-
-impl rustls::client::danger::ServerCertVerifier for NoCertificateVerification {
-    fn verify_server_cert(
-        &self,
-        _end_entity: &CertificateDer<'_>,
-        _intermediates: &[CertificateDer<'_>],
-        _server_name: &ServerName,
-        _ocsp_response: &[u8],
-        _now: UnixTime,
-    ) -> std::result::Result<ServerCertVerified, rustls::Error> {
-        Ok(ServerCertVerified::assertion())
-    }
-
-    fn verify_tls12_signature(
-        &self,
-        _message: &[u8],
-        _cert: &rustls::pki_types::CertificateDer<'_>,
-        _dss: &rustls::DigitallySignedStruct,
-    ) -> Result<HandshakeSignatureValid, rustls::Error> {
-        Ok(HandshakeSignatureValid::assertion())
-    }
-
-    fn verify_tls13_signature(
-        &self,
-        _message: &[u8],
-        _cert: &rustls::pki_types::CertificateDer<'_>,
-        _dss: &rustls::DigitallySignedStruct,
-    ) -> Result<HandshakeSignatureValid, rustls::Error> {
-        Ok(HandshakeSignatureValid::assertion())
-    }
-
-    fn supported_verify_schemes(&self) -> Vec<rustls::SignatureScheme> {
-        vec![
-            rustls::SignatureScheme::ECDSA_NISTP256_SHA256,
-            rustls::SignatureScheme::ECDSA_NISTP384_SHA384,
-            rustls::SignatureScheme::ECDSA_NISTP521_SHA512,
-            rustls::SignatureScheme::ED25519,
-            rustls::SignatureScheme::RSA_PSS_SHA256,
-            rustls::SignatureScheme::RSA_PSS_SHA384,
-            rustls::SignatureScheme::RSA_PSS_SHA512,
-            rustls::SignatureScheme::RSA_PKCS1_SHA256,
-            rustls::SignatureScheme::RSA_PKCS1_SHA384,
-            rustls::SignatureScheme::RSA_PKCS1_SHA512,
-        ]
-    }
-}
-
--- src/postgres/mod.rs
+++ src/postgres/mod.rs
@@ -1,37 +1,151 @@
 
-use std::sync::Arc;
+use std::{env, fs, sync::Arc};
 
-use tokio::io::{AsyncRead, AsyncWrite};
-use tokio_postgres::{NoTls, tls::{MakeTlsConnect, NoTlsStream, TlsConnect, TlsStream}};
+use conf::Conf;
+use deadpool_postgres::{Pool, Runtime, SslMode};
+use rustls::{client::danger::{HandshakeSignatureValid, ServerCertVerified}, pki_types::{CertificateDer, ServerName, UnixTime}};
+use serde::{Deserialize, Serialize};
+use tokio_postgres::NoTls;
 use tokio_postgres_rustls::MakeRustlsConnect;
 
-use crate::{HttpgError, NoCertificateVerification};
+use crate::{HttpgError};
 
-pub enum Conn {
-    NoTls,
-    Tls
+#[derive(Clone, Debug, Conf)]
+#[conf(env_prefix="PG_")]
+pub struct PostgresConfig {
+    #[conf(env)]
+    host: String,
+    #[conf(env)]
+    user: String,
+    #[conf(env, value_parser = |file: &str| -> Result<_, HttpgError> { Ok(fs::read_to_string(file)?) })]
+    password: String,
+    #[conf(env)]
+    dbname: String,
 }
 
-impl<S> MakeTlsConnect<S> for Conn
-    where
-        S: AsyncRead + AsyncWrite + Unpin + Send + Sync + 'static,
-{
-    type Stream: TlsStream;
-    type TlsConnect: TlsConnect<S, Stream = Self::Stream>;
-    type Error = HttpgError;
+impl PostgresConfig {
+    pub fn from_env() -> tokio_postgres::Config {
+        let cfg = Self::parse();
+        tokio_postgres::Config::new()
+            .user(cfg.user)
+            .password(cfg.password)
+            .dbname(cfg.dbname)
+            .host(cfg.host)
+            .ssl_mode(match env::var("PG_SSLMODE").as_deref() {
+                Ok("require") => tokio_postgres::config::SslMode::Require,
+                _ => tokio_postgres::config::SslMode::Prefer,
+            })
+            .to_owned()
+    }
+}
 
-    fn make_tls_connect(&mut self, domain: &str) -> Result<Self::TlsConnect, Self::Error>
-    {
-        match self {
-            Conn::Tls => {
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct DeadPoolConfig {
+    #[serde(default)]
+    pg: deadpool_postgres::Config,
+}
+
+impl DeadPoolConfig {
+    pub fn read() -> Result<Self, config::ConfigError> {
+        let config = config::Config::builder()
+            .add_source(config::Environment::default()
+                .prefix("PG")
+                .separator("_")
+                .keep_prefix(true)
+            )
+            .add_source(config::Environment::default()
+                .prefix("PG_READ")
+                .separator("_")
+                .keep_prefix(false)
+            )
+            .build()?
+        ;
+
+        config.try_deserialize::<Self>()
+    }
+
+    pub fn write() -> Result<Self, config::ConfigError> {
+        let config = config::Config::builder()
+            .add_source(config::Environment::default()
+                .prefix("PG")
+                .separator("_")
+                .keep_prefix(true)
+            )
+            .add_source(config::Environment::default()
+                .prefix("PG_WRITE")
+                .separator("_")
+                .keep_prefix(false)
+            )
+            .build()?
+        ;
+
+        config.try_deserialize::<Self>()
+    }
+
+    pub fn create_pool(&mut self) -> Result<Pool, HttpgError> {
+        match self.pg.ssl_mode {
+            Some(SslMode::Require) | Some(SslMode::Prefer) => {
                 let tls_config = rustls::ClientConfig::builder()
                     .dangerous()
                     .with_custom_certificate_verifier(Arc::new(NoCertificateVerification {}))
                     .with_no_client_auth()
                 ;
-                MakeRustlsConnect::new(tls_config).make_tls_connect("*")
+                let tls = MakeRustlsConnect::new(tls_config);
+
+                self.pg.create_pool(Some(Runtime::Tokio1), tls).map_err(Into::into)
             },
-            Conn::NoTls => NoTls.make_tls_connect("*"),
+            _ =>  self.pg.create_pool(Some(Runtime::Tokio1), NoTls).map_err(Into::into)
         }
     }
 }
+
+
+#[derive(Debug)]
+pub struct NoCertificateVerification {}
+
+impl rustls::client::danger::ServerCertVerifier for NoCertificateVerification {
+    fn verify_server_cert(
+        &self,
+        _end_entity: &CertificateDer<'_>,
+        _intermediates: &[CertificateDer<'_>],
+        _server_name: &ServerName,
+        _ocsp_response: &[u8],
+        _now: UnixTime,
+    ) -> std::result::Result<ServerCertVerified, rustls::Error> {
+        Ok(ServerCertVerified::assertion())
+    }
+
+    fn verify_tls12_signature(
+        &self,
+        _message: &[u8],
+        _cert: &rustls::pki_types::CertificateDer<'_>,
+        _dss: &rustls::DigitallySignedStruct,
+    ) -> Result<HandshakeSignatureValid, rustls::Error> {
+        Ok(HandshakeSignatureValid::assertion())
+    }
+
+    fn verify_tls13_signature(
+        &self,
+        _message: &[u8],
+        _cert: &rustls::pki_types::CertificateDer<'_>,
+        _dss: &rustls::DigitallySignedStruct,
+    ) -> Result<HandshakeSignatureValid, rustls::Error> {
+        Ok(HandshakeSignatureValid::assertion())
+    }
+
+    fn supported_verify_schemes(&self) -> Vec<rustls::SignatureScheme> {
+        vec![
+            rustls::SignatureScheme::ECDSA_NISTP256_SHA256,
+            rustls::SignatureScheme::ECDSA_NISTP384_SHA384,
+            rustls::SignatureScheme::ECDSA_NISTP521_SHA512,
+            rustls::SignatureScheme::ED25519,
+            rustls::SignatureScheme::RSA_PSS_SHA256,
+            rustls::SignatureScheme::RSA_PSS_SHA384,
+            rustls::SignatureScheme::RSA_PSS_SHA512,
+            rustls::SignatureScheme::RSA_PKCS1_SHA256,
+            rustls::SignatureScheme::RSA_PKCS1_SHA384,
+            rustls::SignatureScheme::RSA_PKCS1_SHA512,
+        ]
+    }
+}
+
